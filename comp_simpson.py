156670ae9a4759022983f170d1cb3e2850b0ca02
dutka_bm <brodydutka@gmail.com>
2023-04-18 11:36:29 -0400

    Fixed some things.

--- comp_simpson.py
+++ comp_simpson.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 a = -2
 b = 2
 n = 100
-N = 4
+N = 6
 h = (a+b)/2
 dx = (b-a)/N
 
@@ -36,16 +36,15 @@ ax = fig.add_subplot(1,1,1)
 
 integral = 0
 
-for i in range(N):
+for i in range(int(N/2)):
+    tempA = a + 2*i*dx
+    tempB = a + (2*i+1)*dx
+    tempC = a + (2*i+2)*dx
 
-    tempA = a + i*dx
-    tempB = a + (i+1)*dx
-    tempH = (tempA + tempB)/2
+    x2 = np.linspace(tempA,tempC,n)
+    y2 = simpsons_graph(tempA, tempC, tempB, x2)
 
-    x2 = np.linspace(tempA,tempB,n)
-    y2 = simpsons_graph(tempA, tempB, tempH, x2)
-
-    integral += simpsons(tempA, tempB, tempH) 
+    integral += simpsons(tempA, tempC, tempB) 
 
     plt.plot(x2,y2,'r')
 
@@ -54,6 +53,6 @@ x1 = np.linspace(a,b,n)
 y1 = func(x1)
 plt.plot(x1,y1,'b')
 
-print("The Integral of f(x) is: ", integral)
+print("The Integral of f(x) using Composite Simpson's Rule: ", integral)
 
 plt.show()
